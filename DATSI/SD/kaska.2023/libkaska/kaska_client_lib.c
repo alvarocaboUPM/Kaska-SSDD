b1c019bcaedea10d26200c660e1298bd48eaec7b
Álvaro Cabo <113105033+alvarocaboUPM@users.noreply.github.com>
2023-04-30 17:29:05 +0200

    Fase 3: suscripción (#3)
    * Init
    
    * Disable unused static funcs warning
    
    * Finish entregable
    
    ---------
    
    Co-authored-by: alvarocabo <alvarocaboac2@gmail.com>

--- DATSI/SD/kaska.2023/libkaska/kaska_client_lib.c
+++ DATSI/SD/kaska.2023/libkaska/kaska_client_lib.c
@@ -15,28 +15,41 @@
 #include <unistd.h>
 #define NUM_REQ 3
 
-int client_fd;	  
+int client_fd;
 struct addrinfo *res;
 
+//<TopicName,Offset>
+map *subbed_table;
+
+typedef struct offset
+{
+    int o;
+}Offset;
+
+
 // inicializa el socket y se conecta al servidor
-static int init_socket_client() {    
+static int init_socket_client()
+{
     // socket stream para Internet: TCP
-    if ((client_fd=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    if ((client_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
         perror("error creando socket");
         return -1;
     }
 
-    char *HOST=getenv("BROKER_HOST");
-    char *PORT=getenv("BROKER_PORT");
+    char *HOST = getenv("BROKER_HOST");
+    char *PORT = getenv("BROKER_PORT");
 
     // obtiene la dirección TCP remota
-    if (getaddrinfo(HOST, PORT, NULL, &res)!=0) {
+    if (getaddrinfo(HOST, PORT, NULL, &res) != 0)
+    {
         perror("error en getaddrinfo");
         close(client_fd);
         return -1;
     }
     // realiza la conexión
-    if (connect(client_fd, res->ai_addr, res->ai_addrlen) < 0) {
+    if (connect(client_fd, res->ai_addr, res->ai_addrlen) < 0)
+    {
         perror("error en connect");
         close(client_fd);
         return -1;
@@ -45,17 +58,45 @@ static int init_socket_client() {
     return client_fd;
 }
 
+/**
+ * @brief Frees a subbed_table entry
+ * 
+ * @param k String
+ * @param v Offset
+ */
+static void free_entry(void *k, void *v) {
+    if (v) free(v);
+}
+
+static void print_subbed_map(){
+    map_position*p = map_alloc_position(subbed_table); 
+    map_iter * it = map_iter_init(subbed_table,p);
+    char* key;
+    Offset* o;
+    int i=0;
+
+    puts("");
+    for (; it && map_iter_has_next(it); map_iter_next(it)) 
+    {
+        map_iter_value(it, (const void **) &key, (void **) &o);
+        printf("%d:después de subscribe: nombre %s offset %d\n", i++, key, o->o);
+    }
+    p=map_iter_exit(it);
+}
+
 // inits socket connection before clients main execution
-__attribute__((constructor)) void inicio(void) {
-    if (init_socket_client() < 0) {
+__attribute__((constructor)) void inicio(void)
+{
+    if (init_socket_client() < 0)
+    {
         _exit(1);
     }
 }
 
-
 // Crea el tema especificado.
 // Devuelve 0 si OK y un valor negativo en caso de error.
-int create_topic(char *topic) {
+int create_topic(char *topic)
+{
     int op_code = htonl(0);
     struct iovec iov[3];
     // op code
@@ -70,7 +111,8 @@ int create_topic(char *topic) {
     iov[2].iov_base = topic;
     iov[2].iov_len = topic_len;
 
-    if (writev(client_fd, iov, 3) < 0) {
+    if (writev(client_fd, iov, 3) < 0)
+    {
         perror("error creating topic");
         close(client_fd);
         exit(EXIT_FAILURE);
@@ -81,7 +123,8 @@ int create_topic(char *topic) {
 }
 // Devuelve cuántos temas existen en el sistema y un valor negativo
 // en caso de error.
-int ntopics(void) {
+int ntopics(void)
+{
     int op_code = htonl(1);
     struct iovec iov[1];
 
@@ -89,7 +132,8 @@ int ntopics(void) {
     iov[0].iov_base = &op_code;
     iov[0].iov_len = sizeof(op_code);
 
-    if ((writev(client_fd, iov, 1)) < 0) {
+    if ((writev(client_fd, iov, 1)) < 0)
+    {
         perror("error getting number of topics");
         close(client_fd);
         return -1;
@@ -104,9 +148,10 @@ int ntopics(void) {
 
 /** Envía el mensaje al tema especificado; nótese la necesidad
  de indicar el tamaño ya que puede tener un contenido de tipo binario.
- Devuelve el offset si OK y un valor negativo en caso de error. 
+ Devuelve el offset si OK y un valor negativo en caso de error.
  */
-int send_msg(char *topic, int msg_size, void *msg) {
+int send_msg(char *topic, int msg_size, void *msg)
+{
     int op_code = htonl(2);
     struct iovec iov[5];
     // op code
@@ -128,7 +173,8 @@ int send_msg(char *topic, int msg_size, void *msg) {
     iov[4].iov_base = msg;
     iov[4].iov_len = msg_size;
 
-    if (writev(client_fd, iov, 5) < 0) {
+    if (writev(client_fd, iov, 5) < 0)
+    {
         perror("error publishing a message");
         close(client_fd);
         exit(EXIT_FAILURE);
@@ -139,7 +185,8 @@ int send_msg(char *topic, int msg_size, void *msg) {
 }
 // Devuelve la longitud del mensaje almacenado en ese offset del tema indicado
 // y un valor negativo en caso de error.
-int msg_length(char *topic, int offset) {
+int msg_length(char *topic, int offset)
+{
     int op_code = htonl(3);
     struct iovec iov[4];
     // op code
@@ -157,8 +204,9 @@ int msg_length(char *topic, int offset) {
     int offset_nl = htonl(offset);
     iov[3].iov_base = &offset_nl;
     iov[3].iov_len = sizeof(offset_nl);
-    
-    if (writev(client_fd, iov, 4) < 0) {
+
+    if (writev(client_fd, iov, 4) < 0)
+    {
         perror("error getting msg length");
         close(client_fd);
         exit(EXIT_FAILURE);
@@ -171,7 +219,8 @@ int msg_length(char *topic, int offset) {
 // al del último mensaje enviado más uno y, dado que los mensajes se
 // numeran desde 0, coincide con el número de mensajes asociados a ese tema.
 // Devuelve ese offset si OK y un valor negativo en caso de error.
-int end_offset(char *topic) {
+int end_offset(char *topic)
+{
     int op_code = htonl(4);
     struct iovec iov[3];
     // op code
@@ -185,8 +234,9 @@ int end_offset(char *topic) {
     // topic name
     iov[2].iov_base = topic;
     iov[2].iov_len = topic_len;
-    
-    if (writev(client_fd, iov, 3) < 0) {
+
+    if (writev(client_fd, iov, 3) < 0)
+    {
         perror("error getting num of msgs");
         close(client_fd);
         exit(EXIT_FAILURE);
@@ -198,32 +248,88 @@ int end_offset(char *topic) {
 
 // TERCERA FASE: SUBSCRIPCIÓN
 
+/**
+ * @brief Se suscribe al conjunto de temas recibidos.
+ * No permite suscripción incremental: hay que especificar todos los temas de una vez.
+ * Si un tema no existe o está repetido en la lista simplemente se ignora.
+ *
+ * @param ntopics
+ * @param topics
+ * @return número de temas a los que realmente se ha suscrito | un valor negativo solo si ya estaba suscrito a algún tema.
+ */
+int subscribe(int ntopics, char **topics)
+{
+    if (subbed_table!=NULL){
+        print_subbed_map();
+        return -1;
+    }
 
-// Se suscribe al conjunto de temas recibidos. No permite suscripción
-// incremental: hay que especificar todos los temas de una vez.
-// Si un tema no existe o está repetido en la lista simplemente se ignora.
-// Devuelve el número de temas a los que realmente se ha suscrito
-// y un valor negativo solo si ya estaba suscrito a algún tema.
-int subscribe(int ntopics, char **topics) {
-    return 0;
+    int subbed_topics = 0;
+    subbed_table = map_create(key_string, 0);
+
+    for (int i = 0; i < ntopics; i++)
+    {
+        int repetido,v;
+        Offset* off=(Offset*)malloc(sizeof(Offset));
+
+        char* t_name=strdup(topics[i]);
+        map_get(subbed_table, t_name, &repetido);
+
+        //Existe tema y no duplicado
+        if (repetido<0 && (v = end_offset(t_name))>= 0){
+            off->o=v;
+            map_put(subbed_table, t_name, off);
+            subbed_topics++;
+        }
+    }
+
+    //Debug
+    print_subbed_map();
+
+    return subbed_topics;
 }
 
 // Se da de baja de todos los temas suscritos.
 // Devuelve 0 si OK y un valor negativo si no había suscripciones activas.
-int unsubscribe(void) {
+int unsubscribe(void)
+{
+    if (subbed_table == NULL)
+        return -1;
+    int size = map_size(subbed_table);
+    if (map_destroy(subbed_table, free_entry)<0|| size == 0)
+        return -1;
+    subbed_table=NULL;
     return 0;
 }
 
 // Devuelve el offset del cliente para ese tema y un número negativo en
 // caso de error.
-int position(char *topic) {
-    return 0;
+int position(char *topic)
+{
+    int res;
+    Offset* r = map_get(subbed_table, topic, &res);
+    if (res==0)
+    {
+        res=r->o;
+    }
+
+    return res;
+    
 }
 
 // Modifica el offset del cliente para ese tema.
 // Devuelve 0 si OK y un número negativo en caso de error.
-int seek(char *topic, int offset) {
-    return 0;
+int seek(char *topic, int offset)
+{
+    int res;
+    Offset* r = map_get(subbed_table, topic, &res);
+    if (res==0)
+    {
+        r->o=offset;
+    }
+
+    return res;
+    
 }
 
 // CUARTA FASE: LEER MENSAJES
@@ -232,7 +338,8 @@ int seek(char *topic, int offset) {
 // son de salida.
 // Devuelve el tamaño del mensaje (0 si no había mensaje)
 // y un número negativo en caso de error.
-int poll(char **topic, void **msg) {
+int poll(char **topic, void **msg)
+{
     return 0;
 }
 
@@ -240,13 +347,14 @@ int poll(char **topic, void **msg) {
 
 // Cliente guarda el offset especificado para ese tema.
 // Devuelve 0 si OK y un número negativo en caso de error.
-int commit(char *client, char *topic, int offset) {
+int commit(char *client, char *topic, int offset)
+{
     return 0;
 }
 
 // Cliente obtiene el offset guardado para ese tema.
 // Devuelve el offset y un número negativo en caso de error.
-int commited(char *client, char *topic) {
+int commited(char *client, char *topic)
+{
     return 0;
 }
-
