ffbc56ab4a77e17751208493b0c22036c3a9f791
alvarocabo <alvarocaboac2@gmail.com>
2023-04-30 13:59:18 +0200

    new debuging scripts

--- DATSI/SD/kaska.2023/init.sh
+++ DATSI/SD/kaska.2023/init.sh
@@ -3,10 +3,40 @@
 export BROKER_PORT=12345
 export BROKER_HOST=localhost
 
+# Check if port is already in use
+ps -le | grep broker > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Port $BROKER_PORT is already in use"
+  
+  # Kill the process using the specified port
+  fuser -k -n tcp $BROKER_PORT > /dev/null 2>&1
+  
+  if [ $? -eq 0 ]; then
+    echo "Killed the process running at port $BROKER_PORT"
+  else
+    echo "Failed to kill the process running at port $BROKER_PORT"
+    exit 1
+  fi
+fi
+
 cd broker
 make
+
+# Check if make command was successful
+if [ $? -ne 0 ]; then
+  echo "Make command for BROKER failed with exit code $?"
+  exit 1
+fi
+
+# Run broker in the background and redirect output to a log file
 ./broker $BROKER_PORT &
 
 cd ../clients
 make
-./test
+
+# Check if make command was successful
+if [ $? -eq 0 ]; then
+  ./test
+else
+  echo "Make command for CLIENT failed with exit code $?"
+fi
